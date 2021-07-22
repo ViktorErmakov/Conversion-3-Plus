5925311570329f358bbe0491a7f94e12046a9492
TEENAGER1984 <79091602201@yandex.ru>
2021-07-22 18:08:30 +0300

    добавил сборку разборку в проект

--- Decompile.bat
+++ Decompile.bat
@@ -1,1 +1,2 @@
-precommit1c --decompile D:/Repo/Conversion3.0.1/master/Conversion_3_Plus.cfe D:/Repo/Conversion3.0.1/src
+chcp 65001
+call vrunner decompileext Conversion_3_Plus D:/Repo/Conversion3.0.1/src/Conversion_3_Plus --ibconnection /F"D:\1С\_1C_BASE\Конвертация данных 3.1 (демо)" --db-user Администратор
