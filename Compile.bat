e50131e407d7eaf2756b3d675ae2cd4995976e43
Ермаков Виктор <ermv@1c.ru>
2021-10-25 11:14:35 +0300

    Исправил файлы сборки разборки расширения

--- Compile.bat
+++ Compile.bat
@@ -1,3 +1,2 @@
 chcp 65001
 call vrunner compileexttocfe --src C:/_1C_Repo/Conversion-3-Plus/src/Conversion_3_Plus --out C:/_1C_Repo/Conversion-3-Plus/master/Conversion_3_Plus.cfe --v8version 8.3.19.1229
-@REM call vrunner compileexttocfe --src C:/_1C_Repo/Conversion-3-Plus/src/Conversion_3_Plus --out C:/_1C_Repo/Conversion-3-Plus/master/Conversion_3_Plus.cfe --ibconnection /F'C:\_1C_BASE\Конвертация данных (демо) 3' --db-user Администратор
