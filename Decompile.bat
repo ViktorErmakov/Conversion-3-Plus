d472dd844816b28ba3c7c5bb413bf07f53920c20
Ермаков Виктор <ermv@1c.ru>
2021-11-10 10:15:27 +0300

    поднял версию расширения а так же починил файл разбора расширения

--- Decompile.bat
+++ Decompile.bat
@@ -1,2 +1,2 @@
 chcp 65001
-call vrunner decompileext Conversion_3_Plus ./src/Conversion_3_Plus --ibconnection /F'C:\_1C_BASE\Conversion-3-Plus' --db-user 'администратор' --v8version 8.3.19.1229
+call vrunner decompileext Conversion_3_Plus ./src/Conversion_3_Plus --ibconnection /FC:\_1C_BASE\Conversion-3-Plus --db-user "Администратор" --v8version 8.3.19.1229
