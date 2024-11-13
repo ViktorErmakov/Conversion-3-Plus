b52af1aef993506e8a68c79a3bd580937ef46d12
ViktorErmakov <79091602201@yandex.ru>
2024-11-13 20:55:13 +0300

    Изменил команды сборки, под домашний компьютер
    Пути были со старого компьютера

--- Compile.bat
+++ Compile.bat
@@ -1,2 +1,2 @@
 chcp 65001
-call vrunner compileexttocfe --src C:/_1C_Repo/Conversion-3-Plus/src/Conversion_3_Plus --out C:/_1C_Repo/Conversion-3-Plus/master/Conversion_3_Plus.cfe --v8version 8.3.19.1229
+call vrunner compileexttocfe --src D:/Repo/Conversion-3-Plus/src/Conversion_3_Plus --out D:/Repo/Conversion-3-Plus/master/Conversion_3_Plus.cfe --v8version 8.3.25.1445
