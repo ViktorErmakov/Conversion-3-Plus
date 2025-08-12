2052dfc3e1f73d35cf557bbc9b700b558fa9c2f6
ViktorErmakov <79091602201@yandex.ru>
2025-08-12 22:49:06 +0300

    Разобрал последнюю версию расширения на исходники

--- Decompile.bat
+++ Decompile.bat
@@ -1,2 +1,2 @@
 chcp 65001
-call vrunner decompileext Conversion_3_Plus ./src/Conversion_3_Plus --ibconnection /FD:/Repo/Conversion-3-Plus --db-user "Администратор" --v8version 8.3.25.1445
+call vrunner decompileext Conversion_3_Plus ./src/Conversion_3_Plus --ibconnection /FD:\1С\_1C_BASE\KD3_1 --db-user "Администратор" --v8version 8.3.25.1445
